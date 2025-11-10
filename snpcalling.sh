b4543e7215684d94d064fd0c8c661c0f79ec3e24
inesvieira99 <116963437+inesvieira99@users.noreply.github.com>
2025-11-10 22:22:26 +0000

    snpcalling.sh

--- snpcalling.sh
+++ snpcalling.sh
@@ -16,7 +16,19 @@ MINDEP=8
 
 #======================================================================================
 
-bcftools mpileup -Ou -q 20 -Q 20  -a FORMAT/DP -f $REFERENCE $INPUTFOLDER/$BAM | \
-  bcftools call -mO b -g $MINDEP > $OUTPUTFOLDER/$SAMPLE.unflt.bcf
+MAXDEP=`samtools mpileup -q 20 -Q 20 $INPUTFOLDER/$BAM | cut -f 4 \
+| perl -ne ' $tota+=$_;$n++;print 2.5*$tota/$n,"\n";' | tail -n 1 | cut
+-d'.' -f1`
 
-bcftools index $OUTPUTFOLDER/$SAMPLE.unflt.bcf
+bcftools mpileup -Ov -q 20 -Q 20 -a FORMAT/DP -f $REFERENCE $INPUTFOLDER/
+$BAM | \
+bcftools call -mO b -g $MINDEP,$MAXDEP | \
+bcftools filter -O v -g3 -G10 -s filter311023 -e "QUAL < 20 || FMT/DP >
+$MAXDEP || FMT/DP < 8 || GT='0/1'" | \
+grep -v filter311023 > $OUTPUTFOLDER/$SAMPLE.flt.vcf
+
+
+#bcftools mpileup -Ou -q 20 -Q 20  -a FORMAT/DP -f $REFERENCE $INPUTFOLDER/$BAM | \
+  #bcftools call -mO b -g $MINDEP > $OUTPUTFOLDER/$SAMPLE.unflt.bcf
+
+#bcftools index $OUTPUTFOLDER/$SAMPLE.unflt.bcf
